7b82d0c58b24fd02f9de988e5a16c50133c7e42b
gsoosk <farzadhabibii98@gmail.com>
2020-10-27 20:56:11 +0330

    Fix a bug in If_stage_reg

--- IF_Stage_Reg.v
+++ IF_Stage_Reg.v
@@ -4,13 +4,13 @@ module IF_Stage_Reg (
     input [31:0] instruction_in,
     output reg [31:0] pc, instruction
 );
-    always @(posedge clk, posedge flush) begin
+    always @(posedge clk, posedge rst) begin
         if (rst) 
         begin
             pc <= 32'b0;
             instruction <= 32'b0;
         end
-        else if (flush && ~freeze)
+        else if (clk && flush)
         begin
             pc <= 32'b0;
             instruction <= 32'b0;
@@ -20,7 +20,7 @@ module IF_Stage_Reg (
             pc <= pc_in;
             instruction <= instruction_in;
         end 
-        else if (freeze)
+        else
         begin
             pc <= pc;
             instruction <= instruction;
