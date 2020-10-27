08e3d894505511c1d2d46e414a944658b9098733
gsoosk <farzadhabibii98@gmail.com>
2020-10-27 20:41:28 +0330

    Fix a bug in istruction mem It should not use clock for reading (like RAM)

--- Instruction_Memory.v
+++ Instruction_Memory.v
@@ -4,9 +4,10 @@ module Instruction_Memory(
     output reg[31:0] instruction
 );
     reg[7:0] data[23:0];
-    always @(posedge rst) begin
+    always @(*) begin
         if (rst)
         begin
+            instruction <= 32'b0;
             {data[0], data[1], data[2], data[3]} = 32'b000000_00001_00010_00000_00000000000;
             {data[4], data[5], data[6], data[7]} = 32'b000000_00011_00100_00000_00000000000;
             {data[8], data[9], data[10], data[11]} = 32'b000000_00101_00110_00000_00000000000;
@@ -14,17 +15,10 @@ module Instruction_Memory(
             {data[16], data[17], data[18], data[19]} = 32'b000000_01001_01010_00011_00000000000;
             {data[20], data[21], data[22], data[23]} = 32'b000000_01101_01110_00000_00000000000;
         end
-    end
-    
-
-    always @(posedge rst, posedge clk) begin
-        if (rst)
-        begin
-            instruction <= 32'b0;
-        end
-        if (clk)
+        else
         begin
             instruction <= {data[address],data[address+1],data[address+2],data[address+3]};
         end
     end
+    
 endmodule
