2b0245b60ff546df22cde6ca0b01e7ed6caaee6e
gsoosk <farzadhabibii98@gmail.com>
2020-11-17 21:03:09 +0330

    ADD ALU TB and fix its bugs

--- ALU.v
+++ ALU.v
@@ -5,14 +5,14 @@ module ALU (
    input [31:0] val2,
    input [3:0] exec_cmd,
    input carry_in,
-   output [31:0] alu_res,
+   output reg[31:0] alu_res,
    output [3:0] status
 );
     reg c, v;
     wire z, n;
     always @(*) begin
-        c = 1b'0;
-        v = 1b'0;
+        c = 1'b0;
+        v = 1'b0;
         case (exec_cmd)
             `MOV_ALU:
                 alu_res = val2;
@@ -48,14 +48,67 @@ module ALU (
 
         // Here we use ~ instead of != because the results might be X
         if(exec_cmd == `ADD_ALU || exec_cmd == `ADC_ALU)
-            v = (val1[31] == val2[32]) & (val1[31] == ~alu_res[31]);
+            v = (val1[31] == val2[31]) & (val1[31] == ~alu_res[31]);
         else if (exec_cmd == `SUB_ALU || exec_cmd == `SBC_ALU)
-            v = (val1[31] == ~val2[32]) & (val1[31] == ~alu_res[31]);
+            v = (val1[31] == ~val2[31]) & (val1[31] == ~alu_res[31]);
     end
 
-    assign n = alu_res < 0 ? 1'b1 : 1'b0;
+    assign n = alu_res[31];
     assign z = alu_res == 32'b0 ? 1'b1 : 1'b0;
 
     assign status = {z, c, n, v};
     
+endmodule
+
+
+module ALUTB();
+	reg signed [31:0] a, b;
+	reg[3:0] op;
+    wire [3:0] status;
+    reg c;
+	wire signed [31:0] o;
+    ALU alu(
+        .val1(a), 
+        .val2(b),
+        .exec_cmd(op),
+        .carry_in(c),
+        .alu_res(o),
+        .status(status)
+    );
+
+	initial begin
+	a = -12; b = 20; op = 4'b0; c = 1;
+	#10 op = `MOV_ALU;
+	#10 op = `MVN_ALU;
+	#10 op = `ADD_ALU;
+    	#10 op = `ADC_ALU;
+	#10 op = `SUB_ALU;
+    	#10 op = `SBC_ALU; 
+	#10 op = `AND_ALU;
+    	#10 op = `ORR_ALU;
+    #10 op = `EOR_ALU;
+    #10 op = `CMP_ALU;
+    #10 op = `TST_ALU;
+    #10 op = `LDR_ALU;
+    #10 op = `STR_ALU;
+    // Check V
+    #10 begin
+        a = -2147483647;
+        b = -2147483647;
+        op = `ADD_ALU;
+    end
+    // Check N
+    #10 begin
+        a = 10;
+        b = -21;
+        op = `ADD_ALU;
+    end
+    // Check Z
+    #10 begin
+        a = -10;
+        b = 10;
+        op = `ADD_ALU;
+    end
+	#10 $stop;
+	end
 endmodule
