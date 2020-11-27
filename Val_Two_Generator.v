67e1e7ca65acffccafce95e2ecaba68a0664a133
gsoosk <farzadhabibii98@gmail.com>
2020-11-27 12:29:44 +0330

    Add some wires in val_two_gen * In order to be more readable in debug

--- Val_Two_Generator.v
+++ Val_Two_Generator.v
@@ -13,17 +13,26 @@ module Val_Two_Generator(
     reg [31:0] imm_32_bit, rm_rotate;
     integer i = 0;
 
+    // Helping wires
+    wire [5:0] shift_imm;
+    assign shift_imm = shift_operand[11:7];
 
+    wire [4:0] rotate_imm;
+    assign rotate_imm = shift_operand[11:8];
+
+    wire [1:0] shift;
+    assign shift = shift_operand[6:5];
+ 
     assign result = is_mem_instruction == 1'b1  ? { {20{shift_operand[11]}}, shift_operand} : 
                     immediate == 1'b1           ? imm_32_bit : 
-                    shift_operand[6:5] == `LSL  ? Rm << {shift_operand[11:7]} :
-                    shift_operand[6:5] == `LSR  ? Rm >> {shift_operand[11:7]} :
-                    shift_operand[6:5] == `ASR  ? Rm >> {shift_operand[11:7]} :
-                    shift_operand[6:5] == `ROR  ? rm_rotate : 32'b1;
+                    shift == `LSL               ? Rm << {shift_imm} :
+                    shift == `LSR               ? Rm >> {shift_imm} :
+                    shift == `ASR               ? Rm >> {shift_imm} :
+                    shift == `ROR               ? rm_rotate : 32'b1;
 
     always@(*) begin
         imm_32_bit = {24'b0, shift_operand[7:0]};
-        for(i = 0; i < shift_operand[11:8]; i = i+1) begin
+        for(i = 0; i < rotate_imm; i = i+1) begin
             imm_32_bit = {imm_32_bit[1:0], imm_32_bit[31:2]};
         end    
 
