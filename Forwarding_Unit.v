99f881bbd3ac6cabbb9f2f0c69aac2a15efbb902
gsoosk <farzadhabibii98@gmail.com>
2020-12-18 17:29:15 +0330

    Fix forwarding for when it is disable

--- Forwarding_Unit.v
+++ Forwarding_Unit.v
@@ -3,6 +3,7 @@
 `define FORWARD_FROM_MEM_SEL 2'b10
 
 module Forwarding_Unit (
+    input forwarding_en,
     input [3:0] src1, src2,
     input [3:0] wb_dest, mem_dest,
     input wb_wb_en, mem_wb_en,
@@ -12,40 +13,45 @@ module Forwarding_Unit (
 
     always @(*) begin
         sel_src1 = `FORWARD_FROM_ID_SEL;
-        sel_src1 = `FORWARD_FROM_ID_SEL;
+        sel_src2 = `FORWARD_FROM_ID_SEL;
         forwarded = 1'b0;
 
-        if(mem_wb_en == 1'b1)
+        if(forwarding_en == 1'b1)
         begin
 
-            if(mem_dest == src1)
+            if(mem_wb_en == 1'b1)
             begin
-                forwarded = 1'b1;
-                sel_src1 = `FORWARD_FROM_MEM_SEL;
+
+                if(mem_dest == src1)
+                begin
+                    forwarded = 1'b1;
+                    sel_src1 = `FORWARD_FROM_MEM_SEL;
+                end
+
+                if(mem_dest == src2)
+                begin
+                    forwarded = 1'b1;
+                    sel_src2 = `FORWARD_FROM_MEM_SEL; 
+                end
+
             end
 
-            if(mem_dest == src2)
+            if(wb_wb_en == 1'b1)
             begin
-               forwarded = 1'b1;
-               sel_src2 = `FORWARD_FROM_MEM_SEL; 
-            end
+                if(wb_dest == src1)
+                begin
+                    forwarded = 1'b1;
+                    sel_src1 = `FORWARD_FROM_WB_SEL;
+                end
 
+                if(wb_dest == src2)
+                begin
+                    forwarded = 1'b1;
+                    sel_src2 = `FORWARD_FROM_WB_SEL; 
+                end 
+            end
         end
 
-        if(wb_wb_en == 1'b1)
-        begin
-            if(wb_dest == src1)
-            begin
-                forwarded = 1'b1;
-                sel_src1 = `FORWARD_FROM_WB_SEL;
-            end
-
-            if(wb_dest == src2)
-            begin
-               forwarded = 1'b1;
-               sel_src2 = `FORWARD_FROM_WB_SEL; 
-            end 
-        end
 
     end
     
